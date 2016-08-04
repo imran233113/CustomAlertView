2bea8f23eaca71eab66825f017aba94db47c5202
imran khan <imran233113@gmail.com>
2016-08-04 23:40:52 +0530

    IKAlertView Updated Branch Created

---
+++ CustomAlertView/SecondVC.h
@@ -0,0 +1,13 @@
+//
+//  SecondVC.h
+//  CustomAlertView
+//
+//  Created by Imran Khan on 04/08/16.
+//  Copyright © 2016 IK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondVC : UIViewController
+
+@end
